8ec525fca1de6936bb76798678fc201d78f9c079
Marco Andrey Chmielewski Nunes <marcoandreycnunes@hotmail.com>
2019-11-07 15:47:26 -0200

    + devinho (plugin de utilidades; * ajustes gerais;

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,25 @@
 const path = require('path')
 
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-
+	publicPath: "/",
 	pluginOptions: {
 		'style-resources-loader': {
 			preProcessor: 'stylus',
 			patterns: [
 				path.resolve(__dirname, 'src/assets/stylus/_util.styl')
 			]
+		}
+	},
+	configureWebpack: {
+		resolve: {
+			alias: {
+				"@images": path.resolve(__dirname, 'src/assets/images'),
+				"@stylus": path.resolve(__dirname, 'src/assets/stylus'),
+				"@svgs": path.resolve(__dirname, 'src/assets/svgs'),
+				"@components": path.resolve(__dirname, 'src/components'),
+				"@sections": path.resolve(__dirname, 'src/sections'),
+				"@views": path.resolve(__dirname, 'src/views')
+			}
 		}
 	}
 }
