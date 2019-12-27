8639616f66fd3a270eee6c697c35286f4581f77b
Marco Andrey Chmielewski Nunes <marcoandreycnunes@hotmail.com>
2019-12-27 10:40:43 -0200

    + components de header e footer; * ajustes nos templates de components; - view de home;

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    #footer
+        
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped src="@components/Footer.styl"></style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    #header
+        
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped src="@components/Header.styl"></style>
